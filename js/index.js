3261dc58349c434fbf7bf098954833e8de761521
dportology <davenporta@my.easternct.edu>
2016-03-15 17:59:23 -0400

    web-rtc shiz added

---
+++ js/index.js
@@ -0,0 +1,46 @@
+vendorUrl = window.URL || window.webkitURL;
+
+navigator.getMedia = 	navigator.getUserMedia ||
+						navigator.webkitGetUserMedia ||
+						navigator.mediaDevices.getUserMedia ||
+						navigator.msGetUserMedia;
+
+navigator.getMedia({ video: true, audio: true }, function(stream){
+
+	var Peer = require('simple-peer')
+	var peer = new Peer({
+	    initiator: location.hash === '#init',
+	    trickle: false,
+	    stream: stream
+	})
+
+	peer.on('signal', function(data) {
+	    document.getElementById('yourId').value = JSON.stringify(data)
+	})
+
+	document.getElementById('connect').addEventListener('click', function() {
+	    var otherId = JSON.parse(document.getElementById('otherId').value)
+	    peer.signal(otherId)
+	})
+
+	document.getElementById('send').addEventListener('click', function() {
+	    var yourMessage = document.getElementById('yourMessage').value
+	    peer.send(yourMessage)
+	})
+
+	peer.on('data', function(data) {
+	    document.getElementById('messages').textContent += data + '\n'
+	})
+
+	peer.on('stream', function(stream){
+		var video = document.createElement('video')
+		document.body.appendChild(video)
+
+		video.src = window.URL.createObjectURL(stream)
+		video.play()
+	})
+
+// if streaming doesn't work, don't do any of the above
+}, function (err){
+	console.error(err)
+})
---
+++ js/video.js
@@ -0,0 +1,20 @@
+(function(){
+	var video = document.getElementById('video'),
+		vendorUrl = window.URL || window.webkitURL;
+
+	navigator.getMedia = 	navigator.getUserMedia ||
+							navigator.webkitGetUserMedia ||
+							navigator.mozGetUserMedia ||
+							navigator.msGetUserMedia;
+
+	// Capture Video
+	navigator.getMedia({
+		video: true,
+		audio: false
+	},	function(stream) {
+			video.src = vendorUrl.createObjectURL(stream);
+			video.play();
+	},	function(error){
+			//error code
+	});
+})();
