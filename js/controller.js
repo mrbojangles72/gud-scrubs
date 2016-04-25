9904eea2771fd0577aa0ecff88228e05882f82f2
mrbojangles72 <krockombergera@my.easternct.edu>
2016-04-25 12:07:41 -0400

    Update controller.js
    updated call function with sql/json

--- js/controller.js
+++ js/controller.js
@@ -9,13 +9,25 @@ angular.element(document).ready(function () {
 
 $scope.user = {name:"TUX"};
 $scope.authenticated = true;
+
+$result = mysql_query("SELECT * from listinfo") or die('Could not query');
 	
 });
 
 
 $scope.call = function(){
-
 //make the video call
+
+//connect to db
+$db=mysql_connect($localhost, 'root', 'alex-likes-anaL6969') or die('Could not connect');
+mysql_select_db('carpet_munchers_2', $db) or die('');
+
+
+$result = mysql_query("SELECT CONTACT_NAME,USR_ONLINE,USR_ID FROM ((USR JOIN CONTACT ON (USR.USR_NAME = CONTACT.USR_NAME))) WHERE USR.USR_NAME = "FRED" AND USR.USR_ONLINE = TRUE ORDER BY CONTACT.USR_NAME ASC;") or die('Could not query');
+
+    mysql_close($db);
+    echo json_encode($result);
+
 }
 
 $scope.acceptCall = function(){
